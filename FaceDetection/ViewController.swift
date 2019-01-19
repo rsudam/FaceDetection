b927cbecb2c7d135dbbb009e87f9ffb413900876
rsudam <raghu.sairam@gmail.com>
2019-01-19 11:37:56 +0530

    face detection

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let image = UIImage(named: "image1") else { return }
+        let imageView = UIImageView(image: image)
+        
+        imageView.contentMode = .scaleAspectFit
+        
+        let scaledHeight = view.frame.width / image.size.width * image.size.height
+        imageView.frame = CGRect.init(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
+        
+        view.addSubview(imageView)
+        
+        let request = VNDetectFaceRectanglesRequest.init { (req, err) in
+            if let err = err {
+                print("Failed to detect faces:", err)
+                return
+            }
+            
+            req.results?.forEach({ (res) in
+               
+                DispatchQueue.main.async {
+                    guard let faceObservation = res as? VNFaceObservation  else { return }
+                    
+                    let width = self.view.frame.width * faceObservation.boundingBox.width
+                    let height = scaledHeight * faceObservation.boundingBox.height
+                    let y = scaledHeight * (1 - faceObservation.boundingBox.origin.y) - (height)
+                    let x = self.view.frame.width * faceObservation.boundingBox.origin.x
+                    
+                    let redView = UIView()
+                    redView.backgroundColor = .red
+                    redView.alpha = 0.5
+                    redView.frame = CGRect(x: x, y: y, width: width, height: height)
+                    self.view.addSubview(redView)
+                }
+                
+            })
+        }
+        
+        guard let cgImage = image.cgImage else { return }
+        
+        DispatchQueue.global(qos: .background).async {
+            let handler = VNImageRequestHandler.init(cgImage: cgImage, options: [:])
+            do {
+                try handler.perform([request])
+            } catch let err {
+                print(err)
+            }
+        }
+        
+        
+        
+        
+        
     }
-
 
 }
 
